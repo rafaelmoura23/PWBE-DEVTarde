8124a6e4b072d000c9ee813af1d9ad1f57b4392f
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-15 16:18:35 -0300

    php

---
+++ ProjetoBook/index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">LOGIN BOOK HEAVEN</h3>
+                    <?php
+                    if(isset($_SESSION['nao_autenticado'])):
+                    ?>
+                    <div class="notification is-danger">
+                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['nao_autenticado']);
+                    ?>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ ProjetoBook/painel.php
@@ -0,0 +1,7 @@
+<?php
+include('verifica_login.php');
+?>
+
+<h2>Seja Bem vindo(a) <?php echo $_SESSION['usuario'];?></h2>
+<h2><a href="logout.php">Sair</a></h2>
+
