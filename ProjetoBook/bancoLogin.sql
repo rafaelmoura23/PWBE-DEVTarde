3c4ed4473d56c8dc90b549cb34944f97c3c3d692
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-18 16:47:37 -0300

    produtos

--- ProjetoBook/bancoLogin.sql
+++ ProjetoBook/bancoLogin.sql
@@ -29,7 +29,7 @@ CREATE TABLE `usuario` (
   `nome` varchar(100) NOT NULL,
   `data_cadastro` datetime NOT NULL,
   PRIMARY KEY (`usuario_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'rafael','202cb962ac59075b964b07152d234b70','rafael de','2023-08-17 16:46:33');
+INSERT INTO `usuario` VALUES (1,'rafael','202cb962ac59075b964b07152d234b70','Rafael de Sousa Moura','2023-08-18 15:57:26'),(2,'vinicius','202cb962ac59075b964b07152d234b70','Vinicius','2023-08-18 15:57:37'),(3,'igor','202cb962ac59075b964b07152d234b70','Igor','2023-08-18 15:57:46'),(4,'ezequiel','202cb962ac59075b964b07152d234b70','Ezequiel','2023-08-18 15:57:56'),(5,'eduardo','202cb962ac59075b964b07152d234b70','Eduardo','2023-08-18 15:58:05'),(6,'joao','202cb962ac59075b964b07152d234b70','João','2023-08-18 15:58:14'),(7,'pedro','202cb962ac59075b964b07152d234b70','Pedro','2023-08-18 15:58:24'),(8,'pedro_1','202cb962ac59075b964b07152d234b70','Pedro','2023-08-18 15:58:30'),(9,'maria','202cb962ac59075b964b07152d234b70','Maria','2023-08-18 15:58:37'),(10,'maria1','202cb962ac59075b964b07152d234b70','Maria','2023-08-18 15:58:42'),(11,'celso','202cb962ac59075b964b07152d234b70','Celso','2023-08-18 15:59:01'),(12,'mario','202cb962ac59075b964b07152d234b70','Mario','2023-08-18 15:59:17'),(13,'messi_argentina','202cb962ac59075b964b07152d234b70','Messi','2023-08-18 15:59:25'),(14,'cristiano_ronaldo','202cb962ac59075b964b07152d234b70','Cristiano Ronaldo','2023-08-18 15:59:41'),(15,'jose_1','202cb962ac59075b964b07152d234b70','José','2023-08-18 16:00:12');
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-17 16:50:45
+-- Dump completed on 2023-08-18 16:00:49
