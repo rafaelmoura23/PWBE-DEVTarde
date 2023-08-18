0e8d1d819165ef8e493eae670f50c9320554f162
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-08-18 14:00:05 -0300

    cadastro

--- ProjetoBook/cadastro.php
+++ ProjetoBook/cadastro.php
@@ -8,10 +8,10 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Cadastro - PHP + MySQL - Canal TI</title>
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
+    <title>Cadastro</title>
+    <link rel="stylesheet" type="text/css" href="cadastro.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script> -->
 </head>
 
 <body>
@@ -19,8 +19,7 @@ session_start();
         <div class="hero-body">
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
-    
+                    
                     <?php
                     if(isset($_SESSION['status_cadastro'])):
                     ?>
@@ -45,24 +44,27 @@ session_start();
                     endif;
                     unset($_SESSION['usuario_existe']);
                     ?>
-                    <div class="box">
+
+
+                    <div class="wrapper">
                         <form action="cadastrar.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="nome" type="text" id="pNome" class="input is-large" placeholder="Nome" autofocus>
-                                </div>
+                        <h1>Cadastro</h1>
+                            <div class="input-box">
+                                <input type="text" name="nome" class="input is-large" placeholder="Nome" autofocus>
                             </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input name="usuario" type="text" id="pUsuario" class="input is-large" placeholder="Usuário">
-                                </div>
+                            <div class="input-box">
+                                <input type="text" name="usuario" class="input is-large" placeholder="Usuário">
                             </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" id="pSenha" placeholder="Senha">
-                                </div>
+                            <div class="input-box">
+                                <input type="password" name="senha" class="input is-large" placeholder="Senha">
                             </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth" >Cadastrar</button>
+
+                            <button type="submit" class="btn" >Cadastrar</button>
+
+                            <div class="register-link">
+                                 <p>Just have an account?<a href="login.php">Login</a> </p>
+                            </div>
+
                         </form>
                     </div>
                 </div>
--- ProjetoBook/index.php
+++ ProjetoBook/index.php
@@ -9,7 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <?php
